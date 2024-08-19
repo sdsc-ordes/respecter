b7f4e172324c7277df519a5db13f5fc4b38707f6
Martin Fontanet <martin.fontanet@epfl.ch>
2024-08-19 11:05:10 +0200

    feat: add tests for apply_sparql_query_file

--- tests/test_sparql.py
+++ tests/test_sparql.py
@@ -3,16 +3,8 @@ import rdflib
 
 from respecter.sparql import sparql_query, apply_sparql_query_file
 
-
-SAMPLE_RDF = """
-<https://example.com/Alice> a <https://schema.org/Person> ;
-    <https://schema.org/knowsLanguage> "English" .
-<https://example.com/Bob> a <https://schema.org/Person> ;
-    <https://schema.org/knowsLanguage> "French" .
-
-<https://example.com/SDSC> a <https://schema.org/Organization> ;
-    <https://schema.org/knowsLanguage> "English" .
-"""
+LANGUAGES_DATA_FILE_PATH = "tests/data/languages.ttl"
+SPARQL_FILE_PATH = "tests/sparql/languages.sparql"
 
 SAMPLE_SPARQL = """
 SELECT ?subject WHERE {
@@ -21,13 +13,23 @@ SELECT ?subject WHERE {
 }
 """
 
-def test_sparql_query():
+def load_graph():
     graph = rdflib.Graph()
-    graph.parse(data=SAMPLE_RDF, format="ttl")
+    graph.parse(LANGUAGES_DATA_FILE_PATH, format="ttl")
+    return graph
+
+def test_sparql_query():
+    graph = load_graph()
     query_results = sparql_query(graph, SAMPLE_SPARQL)
-    
     bindings = query_results.get("results").get("bindings", []) 
     
     assert len(bindings) == 1
     assert bindings[0].get("subject", {}).get("value", "") == "https://example.com/Alice"
 
+def test_apply_sparql_query():
+    graph = load_graph()
+    query_results = apply_sparql_query_file(graph, SPARQL_FILE_PATH)
+    bindings = query_results.get("results").get("bindings", [])
+
+    assert len(bindings) == 1
+    assert bindings[0].get("subject", {}).get("value", "") == "https://example.com/Alice"
