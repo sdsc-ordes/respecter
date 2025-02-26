294abb74f88fced2763022317e815eb389ca591d
cmdoret <cyril.mattheydoret@gmail.com>
2025-02-26 01:36:28 +0100

    feat: constants for default configs location [WIP]

---
+++ src/respecter/defaults.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+DEFAULT_CONFIG_PATH = Path(__file__).parent / "config" / "sparql.yaml"
+DEFAULT_QUERY_PATH = Path(__file__).parent / "sparql_queries" / "sparql_query.sparql"
