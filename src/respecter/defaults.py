f415db15ea46e90740d3bbb031fb9454f4399520
cmdoret <cyril.mattheydoret@gmail.com>
2025-02-27 16:39:18 +0100

    fix(defaults): cleaner resource access

--- src/respecter/defaults.py
+++ src/respecter/defaults.py
@@ -1,5 +1,13 @@
 from importlib import resources
 
-CONFIG = resources.read_text("respecter", "data/config.yaml")
-QUERY = resources.read_text("respecter", "data/queries/ontology_metadata.sparql")
-TEMPLATE = resources.read_text("respecter", "data/template.html")
+
+def _read_resource(path: str):
+    data = (
+        resources.files("respecter").joinpath(path).open("r", encoding="utf-8").read()
+    )
+    return data
+
+
+CONFIG = _read_resource("data/config.yaml")
+QUERY = _read_resource("data/queries/ontology_metadata.sparql")
+TEMPLATE = _read_resource("data/template.html")
